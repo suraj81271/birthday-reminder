89ab385b6b8529a35cd3ee95383e8089da9b0b53
Suraj <surajsingh81271@gmail.com>
2021-04-13 19:08:15 +0530

    Updates

---
+++ precache-manifest.f409e5a7a0254363b531d7f2ebe2a276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9bb43420872a543af033468c014d29",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "d9d17932273fd9a0365c",
+    "url": "/birthday-reminder/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d17932273fd9a0365c",
+    "url": "/birthday-reminder/static/js/main.f73b0b26.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
