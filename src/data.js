06ac3681f5c0d3f6d421021d716cb82979a79f73
Suraj <surajsingh81271@gmail.com>
2021-04-13 19:14:06 +0530

    ready to deploy on github

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Suraj Singh",
+    age: 25,
+    image:
+      "https://scontent.fdel13-1.fna.fbcdn.net/v/t1.0-9/s960x960/155118977_1154111018343460_499679114106454396_o.jpg?_nc_cat=106&ccb=1-3&_nc_sid=730e14&_nc_ohc=aqINlHEX1D0AX8S7Akw&_nc_ht=scontent.fdel13-1.fna&tp=7&oh=b39ae458a94cdca085ecf93351de0f8b&oe=6077963E",
+  },
+  {
+    id: 2,
+    name: "Nitish",
+    age: 24,
+    image:
+      "https://scontent.fdel13-1.fna.fbcdn.net/v/t1.0-9/p843x403/36317852_645898519096688_3361949654546644992_o.jpg?_nc_cat=102&ccb=1-3&_nc_sid=19026a&_nc_ohc=Tk_cOHYSR6sAX-Cge_v&_nc_ht=scontent.fdel13-1.fna&tp=6&oh=25993035aa7875a7c0f5cee248ac375d&oe=6079BEC5",
+  },
+  {
+    id: 3,
+    name: "Ashish Mishra",
+    age: 26,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Vikas Yadav",
+    age: 24,
+    image:
+      "https://scontent.fdel13-1.fna.fbcdn.net/v/t1.0-1/c0.11.320.320a/p320x320/43663884_2156519224570405_4222756169766141952_o.jpg?_nc_cat=104&ccb=1-3&_nc_sid=7206a8&_nc_ohc=nO9b1rTyhq0AX9s3KLK&_nc_ht=scontent.fdel13-1.fna&tp=27&oh=20a3532b17e06f9be65aeb63976f0993&oe=6078BBF9",
+  },
+  {
+    id: 5,
+    name: "Mridul Tripathi",
+    age: 25,
+    image:
+      "https://scontent.fdel13-1.fna.fbcdn.net/v/t1.0-1/p320x320/72890575_2394434830776623_1481384879529656320_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=7206a8&_nc_ohc=esvCgfaFwUMAX-jC9-2&_nc_ht=scontent.fdel13-1.fna&tp=6&oh=43e92ce63b8ca623b4a81215bb65a1e5&oe=60787F67",
+  },
+];
